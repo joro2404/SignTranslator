f9c38901dc9af7f437164a951f482eb5c412a643
tonipelovski <tonipelovski@gmail.com>
2020-10-02 21:10:06 +0300

    sending img as base64

--- SignRecogApp/app/src/main/java/com/example/signrecogapp/CameraPreview.java
+++ SignRecogApp/app/src/main/java/com/example/signrecogapp/CameraPreview.java
@@ -1,22 +1,62 @@
 package com.example.signrecogapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.net.http.AndroidHttpClient;
 import android.os.Environment;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
+import android.util.Base64;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 class Preview extends SurfaceView implements SurfaceHolder.Callback {
     SurfaceHolder mHolder;
@@ -33,14 +73,85 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
+    public Allocation renderScriptNV21ToRGBA8888(Context context, int width, int height, byte[] nv21) {
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs));
+
+        Type.Builder yuvType = new Type.Builder(rs, Element.U8(rs)).setX(nv21.length);
+        Allocation in = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT);
+
+        Type.Builder rgbaType = new Type.Builder(rs, Element.RGBA_8888(rs)).setX(width).setY(height);
+        Allocation out = Allocation.createTyped(rs, rgbaType.create(), Allocation.USAGE_SCRIPT);
+
+        in.copyFrom(nv21);
+
+        yuvToRgbIntrinsic.setInput(in);
+        yuvToRgbIntrinsic.forEach(out);
+        return out;
+    }
+
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, acquire the camera and tell it where
         // to draw.
 
         Camera.PreviewCallback previewCallback = new Camera.PreviewCallback()
         {
-            public void onPreviewFrame(byte[] data, Camera camera)
-            {
+            public void onPreviewFrame(byte[] data, Camera camera) {
+
+                Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+                Allocation bmData = renderScriptNV21ToRGBA8888(
+                        getContext(), 1920, 1080, data);
+                bmData.copyTo(bitmap);
+
+                File imagefile = new File(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_MOVIES)
+                        , "/" + "100" + ".jpg");
+                FileInputStream fis = null;
+                try{
+                    fis = new FileInputStream(imagefile);
+                }catch(FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                Bitmap bm = BitmapFactory.decodeStream(fis);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] imageBytes = baos.toByteArray();
+                final String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+
+
+                final HttpClient httpclient;
+                final HttpPost httpPost;
+                ArrayList<NameValuePair> postParameters;
+                httpclient = new DefaultHttpClient();
+                httpPost = new HttpPost("http://78.130.176.59/server/decode.php");
+
+
+                postParameters = new ArrayList<NameValuePair>();
+                postParameters.add(new BasicNameValuePair("param1", encodedImage + "/n"));
+
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(postParameters, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            HttpResponse response = httpclient.execute(httpPost);
+                            Log.v(response.toString(), encodedImage);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+
                 Camera.Parameters parameters = camera.getParameters();
                 int imageFormat = parameters.getPreviewFormat();
                 if (imageFormat == ImageFormat.NV21)
@@ -48,7 +159,7 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 
                     int w = parameters.getPreviewSize().width;
                     int h = parameters.getPreviewSize().height;
-
+/*
                     Rect rect = new Rect(0, 0, w, h);
                     YuvImage img = new YuvImage(data, ImageFormat.NV21, w, h, null);
                     OutputStream outStream = null;
@@ -76,6 +187,9 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
                     {
                         e.printStackTrace();
                     }
+
+*/
+
                 }
             }
 
@@ -89,7 +203,10 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
             e.printStackTrace();
         }
         mCamera.setPreviewCallback(previewCallback);
+
+
     }
+
 
     public void surfaceDestroyed(SurfaceHolder holder) {
         // Surface will be destroyed when we return, so stop the preview.
@@ -105,6 +222,8 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         // the preview.
         Camera.Parameters parameters = mCamera.getParameters();
         List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
 
         // You need to choose the most appropriate previewSize for your app
         //Camera.Size previewSize = previewSizes.get(15);
