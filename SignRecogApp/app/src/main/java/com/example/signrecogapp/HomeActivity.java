1667114c0fcf1f8efe8e1dee567a6fedce732e7e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 11:51:28 +0300

    Added home view manifest

--- SignRecogApp/app/src/main/java/com/example/signrecogapp/HomeActivity.java
+++ SignRecogApp/app/src/main/java/com/example/signrecogapp/HomeActivity.java
@@ -1,4 +1,26 @@
 package com.example.signrecogapp;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
