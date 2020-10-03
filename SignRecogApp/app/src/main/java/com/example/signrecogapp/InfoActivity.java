780c77718ac39d9d20d21a174ee1bdd443fea60c
pgmatev <petargabriel.matev@gmail.com>
2020-10-03 23:53:43 +0300

    Started work on Info Page

--- SignRecogApp/app/src/main/java/com/example/signrecogapp/InfoActivity.java
+++ SignRecogApp/app/src/main/java/com/example/signrecogapp/InfoActivity.java
@@ -12,6 +12,6 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_info);
     }
 }
