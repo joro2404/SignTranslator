1a2a284462e85e2bc91b83c356a799af3145bc40
joro2404 <joro2404@abv.bg>
2020-10-04 17:13:32 +0300

    parsing img

--- ml/model.py
+++ ml/model.py
@@ -12,11 +12,15 @@ model.eval()
 
 data = cv2.imread("./source/iloveyou.jpg")
 data = cv2.resize(data , (224, 224))
+# print(data)
 data = cv2.dnn.blobFromImage(data) #this is (1, 3, 224, 224) shaped image
-data = np.array([data])
+data = np.repeat(data, 50, axis=0).reshape(-1, 1, 3, 224, 224)
+data = np.moveaxis(data, 0, 2)
 data = torch.from_numpy(data)
+print(data.shape)
 # prepare_input(data)
 # print(data)
 
 a = model(data)
-print(model.Conv3d_1a_7x7)
+print(np.argmax(a['logits'].detach().numpy()[0]))
+
