78f2d67693ab74e45354aa3ed4f7c8649f2aa87b
Martin Jordanov <xxm0703@abv.bg>
2020-10-04 23:17:11 +0300

    16 frames in request handled

--- ml/api_endpoint.py
+++ ml/api_endpoint.py
@@ -3,17 +3,23 @@
 import json
 from flask import Flask, request
 from base64 import decodebytes
+from model import Translator
 
 app = Flask(__name__)
+t = Translator()
+
+
+def base_to_jpg(key):
+    form_data = request.form
+    image_base64 = form_data[key]
+    str_to_bytes = str.encode(image_base64)
+    return decodebytes(str_to_bytes)
 
 
 @app.route('/', methods=['POST'])
 def index():
-    image_base64 = request.form['param1']
-    str_to_bytes = str.encode(image_base64)
-    image = decodebytes(str_to_bytes)
-    with open("foo.jpg", "wb") as f:
-        f.write(image)
+    vod = [base_to_jpg('param' + str(i)) for i in range(16)]
+
     return json.dumps({'name': 'alice',
                        'email': 'alice@outlook.com'})
 
