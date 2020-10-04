3761457a372dea724972902313510a6df738dc9b
Martin Jordanov <xxm0703@abv.bg>
2020-10-04 21:19:59 +0300

    Working ML...almost

--- ml/model.py
+++ ml/model.py
@@ -6,18 +6,21 @@ import numpy as np
 
 frames = []
 vidcap = cv2.VideoCapture('./source/book.mp4')
+
+
 def getFrame(sec):
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-    hasFrames,image = vidcap.read()
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+    hasFrames, image = vidcap.read()
     if hasFrames:
         image = cv2.resize(image, (224, 224))
         image = cv2.dnn.blobFromImage(image)
         frames.append(image)
     return hasFrames
 
+
 sec = 0
-frameRate = 0.04 #//it will capture image in each 0.04 second
-count=1
+frameRate = 0.04  # it will capture image in each 0.04 second
+count = 1
 success = getFrame(sec)
 
 while success:
@@ -25,7 +28,6 @@ while success:
     sec = sec + frameRate
     sec = round(sec, 2)
     success = getFrame(sec)
-
 
 while len(frames) > 50:
     frames.pop()
@@ -50,24 +52,19 @@ model.eval()
 torch.no_grad()
 
 data = cv2.imread("./source/nine.jpg")
-# print(data.shape)
-data = cv2.resize(data , (224, 224))
-# print(data.shape)
-# print(data)
-data = cv2.dnn.blobFromImage(data) #this is (1, 3, 224, 224) shaped image
-# print(data.shape)
+data = cv2.resize(data, (224, 224))
+data = cv2.dnn.blobFromImage(data)  # this is (1, 3, 224, 224) shaped image
+
 data = np.repeat(data, 50, axis=1).reshape((-1, 1, 3, 224, 224))
 data = np.moveaxis(data, 0, 2)
 data = torch.from_numpy(data)
+
 print('data:', data.shape)
-
-
-a = [x for x in labels['words'] if list(x)[0] == 't']
-print(labels['words_to_id']['three'])
-print(labels['words'][1800:1840])
-print(a)
 
 # a = [x for x in labels['words'] if list(x)[0] == 't']
 
 a = model(data)
-print(a)
+# print(a)
+word_index = np.argmax(a['logits'].detach().numpy()[0])
+print(word_index)
+print(labels['words'][word_index])
