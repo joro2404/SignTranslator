f7c1e90dc717c70c1f87a7c0e9fc1d02eca60a33
tonipelovski <tonipelovski@gmail.com>
2020-10-04 13:43:36 +0300

    camera preview ui changes

--- SignRecogApp/app/src/main/java/com/example/signrecogapp/MainActivity.java
+++ SignRecogApp/app/src/main/java/com/example/signrecogapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.signrecogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
@@ -10,11 +12,14 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
         topView.setBackgroundColor(getResources().getColor(android.R.color.black));
         addContentView(topView, layoutParams);
-
+        float d = getApplicationContext().getResources().getDisplayMetrics().density;
         ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) topView.getLayoutParams();
-        marginLayoutParams.setMargins(0, 200, 0, 0);
+        marginLayoutParams.setMargins(0, (int) (100*d), 0, 0);
 
         View bottomView = new View(this);
         layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
@@ -60,7 +65,48 @@ public class MainActivity extends AppCompatActivity {
         addContentView(bottomView, layoutParams);
 
         marginLayoutParams = (ViewGroup.MarginLayoutParams) bottomView.getLayoutParams();
-        marginLayoutParams.setMargins(0, 800, 0, 0);
+        marginLayoutParams.setMargins(0, (int) (350*d), 0, 0);
+
+
+        TextView textView = new TextView(this);
+        layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.height = ViewGroup.LayoutParams.FILL_PARENT;
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        textView.setBackgroundColor(getResources().getColor(R.color.trBlack));
+        addContentView(textView, layoutParams);
+
+        textView.setText("Lorem Ipsum");
+        textView.setTextColor(getResources().getColor(R.color.white));
+        marginLayoutParams = (ViewGroup.MarginLayoutParams) textView.getLayoutParams();
+        marginLayoutParams.setMargins(0, (int) (450*d), 0, 0);
+
+
+        Button button = new Button(this);
+
+
+        layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.height = (int) (45*d);
+        layoutParams.width = (int) (45*d);
+
+        button.setBackground(getResources().getDrawable(R.drawable.ellipse_1));
+        addContentView(button, layoutParams);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        button.setTranslationX((width/2) - ((45*d)/2));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            button.setTextAppearance(R.style.some_id);
+        }
+
+        button.setText(R.string.back_button);
+        marginLayoutParams = (ViewGroup.MarginLayoutParams) button.getLayoutParams();
+        marginLayoutParams.setMargins(0, (int) (600*d), 0, 0);
+
+
 
     }
 
