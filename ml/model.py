599c39cc80f30a5c87f561e211ab8041ae2d3748
joro2404 <joro2404@abv.bg>
2020-10-04 14:44:05 +0300

    demo not working

--- ml/model.py
+++ ml/model.py
@@ -2,108 +2,7 @@ import torch
 import i3d
 import numpy as np
 import cv2
-import scipy.misc
-from PIL import Image
-import scipy.ndimage
 
-def to_numpy(tensor):
-    if torch.is_tensor(tensor):
-        return tensor.cpu().numpy()
-    elif type(tensor).__module__ != "numpy":
-        raise ValueError(f"Cannot convert {type(tensor)} to numpy array")
-    return tensor
-
-
-def im_to_numpy(img):
-    img = to_numpy(img)
-    img = np.transpose(img, (1, 2, 0))  # H*W*C
-    return img
-
-
-def resize_generic(img, oheight, owidth, interp="bilinear", is_flow=False):
-    """
-    Args
-    inp: numpy array: RGB image (H, W, 3) | video with 3*nframes (H, W, 3*nframes)
-          |  single channel image (H, W, 1) | -- not supported:  video with (nframes, 3, H, W)
-    """
-
-    # resized_image = cv2.resize(image, (100, 50))
-    ht, wd, chn = img.shape[0], img.shape[1], img.shape[2]
-    if chn == 1:
-        # print(type(img))
-        resized_img = np.array(Image.fromarray(img).resize(
-            img.squeeze(), [oheight, owidth], interp=interp, mode="F"
-        ).reshape(oheight, owidth, chn))
-    elif chn == 3:
-        # resized_img = scipy.misc.imresize(img, [oheight, owidth], interp=interp)  # mode='F' gives an error for 3 channels
-        resized_img = cv2.resize(img, (owidth, oheight))  # inverted compared to scipy
-    elif chn == 2:
-        # assert(is_flow)
-        resized_img = np.zeros((oheight, owidth, chn), dtype=img.dtype)
-        for t in range(chn):
-            # resized_img[:, :, t] = scipy.misc.imresize(img[:, :, t], [oheight, owidth], interp=interp)
-            # resized_img[:, :, t] = scipy.misc.imresize(img[:, :, t], [oheight, owidth], interp=interp, mode='F')
-            # resized_img[:, :, t] = np.array(Image.fromarray(img[:, :, t]).resize([oheight, owidth]))
-            resized_img[:, :, t] = scipy.ndimage.interpolation.zoom(
-                img[:, :, t], [oheight, owidth]
-            )
-    else:
-        in_chn = 3
-        # Workaround, would be better to pass #frames
-        if chn == 16:
-            in_chn = 1
-        if chn == 32:
-            in_chn = 2
-        nframes = int(chn / in_chn)
-        img = img.reshape(img.shape[0], img.shape[1], in_chn, nframes)
-        resized_img = np.zeros((oheight, owidth, in_chn, nframes), dtype=img.dtype)
-        for t in range(nframes):
-            frame = img[:, :, :, t]  # img[:, :, t*3:t*3+3]
-            frame = cv2.resize(frame, (owidth, oheight)).reshape(
-                oheight, owidth, in_chn
-            )
-            # frame = scipy.misc.imresize(frame, [oheight, owidth], interp=interp)
-            resized_img[:, :, :, t] = frame
-        resized_img = resized_img.reshape(
-            resized_img.shape[0], resized_img.shape[1], chn
-        )
-
-    if is_flow:
-        # print(oheight / ht)
-        # print(owidth / wd)
-        resized_img = resized_img * oheight / ht
-    return resized_img
-
-def prepare_input(
-    rgb: torch.Tensor,
-    resize_res: int = 256,
-    inp_res: int = 224,
-    mean: torch.Tensor = 0.5 * torch.ones(3), std=1.0 * torch.ones(3),
-):
-    """
-    Process the video:
-    1) Resize to [resize_res x resize_res]
-    2) Center crop with [inp_res x inp_res]
-    3) Color normalize using mean/std
-    """
-    iC, iF, iH, iW = rgb.shape
-    rgb_resized = np.zeros((iF, resize_res, resize_res, iC))
-    for t in range(iF):
-        tmp = rgb[:, t, :, :]
-        tmp = resize_generic(
-            im_to_numpy(tmp), resize_res, resize_res, interp="bilinear", is_flow=False
-        )
-        rgb_resized[t] = tmp
-    rgb = np.transpose(rgb_resized, (3, 0, 1, 2))
-    # Center crop coordsim_to_torch
-    ulx = int((resize_res - inp_res) / 2)
-    uly = int((resize_res - inp_res) / 2)
-    # Crop 256x256
-    rgb = rgb[:, :, uly : uly + inp_res, ulx : ulx + inp_res]
-    rgb = to_torch(rgb).float()
-    assert rgb.max() <= 1
-    rgb = color_normalize(rgb, mean, std)
-    return rgbim_to_torch
 
 
 model = i3d.InceptionI3d(num_in_frames=50)
@@ -114,9 +13,9 @@ model.eval()
 data = cv2.imread("./source/iloveyou.jpg")
 data = cv2.resize(data , (224, 224))
 data = cv2.dnn.blobFromImage(data) #this is (1, 3, 224, 224) shaped image
-
+data = np.array([data])
 data = torch.from_numpy(data)
-prepare_input(data)
+# prepare_input(data)
 # print(data)
 
 a = model(data)
